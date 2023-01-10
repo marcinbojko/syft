15537e792f71f5c63facc8e4fe29b6caadeda2d7
Marcin Bojko <marcin@bojko.com.pl>
2023-01-10 19:20:29 +0100

    Version 0.65.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName        = 'syft'
-$version            = '0.64.0'
+$version            = '0.65.0'
 $url64              = "https://github.com/anchore/syft/releases/download/v"+$version+"/syft_"+$version+"_windows_amd64.zip"
-$checksum64         = 'e86a08188610b8df458fc8000916aca72c78f3ae1c1c63372deab67c0269d006'
+$checksum64         = '4f3954a7e7a58acd64e15a8a16be3d08cbd1edcee78afad3ca907161008cddab'
 $toolsDir           = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
